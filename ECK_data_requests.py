ef3a06fefd5cb05101b65b03874d9bfcac0cb909
Claramane <107230919+Claramane@users.noreply.github.com>
2023-03-20 11:08:47 +0800

    Update ECK_data_requests.py
    增加影像查詢功能

--- ECK_data_requests.py
+++ ECK_data_requests.py
@@ -52,7 +52,7 @@ def scrape_data(ChartNo):
 
     print(patient_info)
 
-    return soup
+    return soup, patient_info
 
 labdata_key_dict = {
     '動態產生eGFR': 'CeGFR', 'Albumin': 'C6309038', 'Alk-P': 'C6309027', 'Amylase': 'C6309017', 'BE(B)': 'C6BEB', 'BNP': 'C6312193', 'BUN': 'C6309002', 'CA': 'C6309011', 'Cholesterol-T': 'C6309001', 'CK-MB': 'C6399086', 'CPK': 'C6309032', 'Creatinine': 'C6309015', 'Creatinine (U)': 'C6309016', 'CRP': 'C6312903', 'CTCO2': 'C7CTCO2', 'Glucose': 'C5GLUCO', 'Glucose,AC': 'C6309901', 'GOT (AST)': 'C6309025', 'GPT (ALT)': 'C6309026', 'HbA1C': 'C6309006', 'HCO3-ACT': 'C4HCO3A', 'HDL-C': 'C6309043', 'Iron': 'CIRON', 'K': 'C6309022', 'LDL-C': 'C6309044', 'Lipase': 'C6309064', 'Microalbumin (Nephelometry)': 'C6312111', 'NA': 'C6309021', 'O2SAT': 'C8O2SAT', 'pCO2': 'C3PCO2', 'pH': 'C3PH', 'pO2': 'C2PO2', 'r-GT': 'C6309031', 'T-Bilirubin': 'C6309029', 'TIBC': 'CTIBC', 'Triglyceride': 'C6309004', 'Troponin I': 'C6399035', 'Uric Acid': 'C6309013', 'Urine microalbumin/creatinine ratio': 'C6300012', 'A-Lym': 'C7ALYM', 'ABO Typing  自述血型:(     )': 'C6311001', 'APTT': 'C6308036', 'Band': 'C6BAND', 'Baso': 'C5BA', 'Blast': 'C8BLAST', 'Eosin': 'C4EO', 'Hb': 'C6308003', 'Ht': 'C6308004', 'Lym': 'C2LY', 'MCH': 'CMCH', 'MCHC': 'CMCHC', 'MCV': 'CMCV', 'Metamyelo': 'CAMETAMY', 'Mono': 'C3MO', 'Myelocyte': 'CBMYELO', 'Neutro': 'C1NE', 'NRBC': 'CCNRBC', 'Other': 'CUOTHER', 'Platelet': 'C6308006', 'Promyelo': 'C9PROMYE', 'PT': 'C6308026', 'RBC': 'C6308001', 'RH(D)': 'C6311003', 'WBC': 'C6308002', 'Anti-HAV IgM': 'C6314039', 'Anti-HBc': 'C6314037', 'Anti-HBs': 'C6314033', 'Anti-HCV': 'C6314051', 'Ferritin': 'C6327017', 'HBsAg': 'C6314032', 'Bacteria/HPF': 'CW4BACT', 'Bacteria/uL': 'CW4BACT1', 'Bilirubin': 'C8BILI', 'Cast': 'CWCAST', 'Clarity': 'C2CLARI', 'Color': 'C1COLOR', 'Crystal': 'CWCRYSTA', 'Epith.cell/HPF': 'CW3EPITH', 'Epith.cell/uL': 'CW3EPIT1', 'Fungi': 'CWFUNGI', 'Ketone': 'C6KETON', 'Leukocyte': 'CCLEUKO', 'Mucus': 'CW5MUCUS', 'Nitrite': 'CBNITR', 'OB': 'C9OB', 'Protein': 'C4PROTE', 'R.B.C./HPF': 'CW1RBC', 'R.B.C./uL': 'CW1RBC1', 'SP.GR.': 'C7SPGR', 'Stool-O.B.(定量免疫法)': 'C6399121', 'Synovial Crystal (關節液偏光檢查)': 'C6316013', 'Trichomonas': 'CWTRICHO', 'Urobilinogen': 'CAUROB', 'W.B.C./HPF': 'CW2WBC', 'W.B.C./uL': 'CW2WBC1', 'Blood culture': 'C6313016', 'Influenza A Ag': 'C6314065', 'Influenza B Ag': 'C6314066'
@@ -92,6 +92,7 @@ def get_all_labdata():
     all_data = json.dumps(dict, indent=4, ensure_ascii=False).encode('utf-8')
     all_data = all_data.decode()
     print(all_data)
+    return all_data
 
 
 def get_last_labdata():
@@ -126,27 +127,40 @@ def get_simple_labdata():
     simple_data = json.dumps(dict_simple, indent = 4)
     print(simple_data)
 
+def get_image():
+    dict_img = {}
+    trs = soup.find_all('tr', attrs={'data-xrayhead': re.compile("^p")})
+    for i in trs: 
+        accessionNumber = str(i.find_all("td")[0].text)
+        exam_name = str(i.find_all("td")[1].text).strip()
+        time = str(i.find_all("td")[2].text)
+        img_url = f"http://172.23.0.10/html5/ShowImage.html?psHash=12BA22714218875BF2492235060CAAD3:1:20230321:1003071042:0009485318:08:&accessionNumber={accessionNumber}"
+        dict = {
+            "accessionNumber": accessionNumber,
+            "img_url": img_url,
+            "time": time
+        }
+        dict_img[exam_name] = dict
+    dict_img = json.dumps(dict_img, indent = 4, ensure_ascii=False).encode('utf-8')
+    dict_img = dict_img.decode()
+    print(dict_img)
+    
 
 
-
-# for labdata_name, labdata_code in labdata_key_dict.items():
-#     # print(labdata_code)
-#     try: 
-#         print(labdata_name)
-#         get_last_labdata(labdata_code)
-#     except:
-#         continue
 
 # while True:
     # 病例號放這邊，補0到十個數字
     # 48251
     # ChartNo = input("請輸入病例號: ")
-ChartNo = "5426687"
+ChartNo = "9485318"
 t1 = time.time()
-soup = scrape_data(ChartNo)
+soup, patient_info = scrape_data(ChartNo)
+get_image()
 # get_simple_labdata()
-get_last_labdata()
-# get_all_labdata()
+# get_last_labdata()
+# data = get_all_labdata()
+# data = json.loads(data)
+# print(data["Hb"])
 
 
 t2 = time.time()
